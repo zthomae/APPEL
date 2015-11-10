437cd02ff9f28356685302d0853b230a356dc83b
Zach Thomae <zthomae@gmail.com>
2015-11-10 00:27:03 -0600

    Refactor more

--- interpreter.hs
+++ interpreter.hs
@@ -5,11 +5,11 @@
 opI :: (a -> a -> a) -> [a] -> [a] -> [a]
 opI f xs (y:[]) = map (\x -> f x y) xs
 opI f (x:[]) ys = map (\y -> f x y) ys
-opI f x y = opI' f x y
-opI' f [] [] = []
-opI' f [] _ = error "Mismatched list lengths"
-opI' f _ [] = error "Mismatched list lengths"
-opI' f (x:xs) (y:ys) = (f x y : opI' f xs ys)
+opI f x y =
+    let opI' f (x:xs) (y:ys) l = opI' f xs ys (l ++ [f x y]) 
+        opI' f [] [] l = l
+        opI' f _ _ _ = error "Mismatched list lengths"
+    in  opI' f x y []
 
 addI = opI (+)
 subI = opI (-)
@@ -21,10 +21,10 @@ divI = opI div
 reshape :: Int -> Int -> [a] -> [[a]]
 reshape 0 _ _ = []
 reshape x y t =
-    let iter 0 _ l = []
-        iter x y l = [take y l] ++ reshape (x-1) y (drop y l)
+    let reshape' ct 0 l = l
+        reshape' ct x l = reshape' (drop y ct) (x-1) (l ++ [take y ct])
     in  case (signum(x), signum(y)) of
-             (1, 1) -> iter x y (cycle t)
+             (1, 1) -> reshape' (cycle t) x []
              (_, _) -> error "Invalid dimensions"
 
 -- dimT: Get the dimensions of a table
