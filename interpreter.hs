3e38bdf6fc3c8a823b7311011c454caeede899e3
Zach Thomae <zthomae@gmail.com>
2015-11-09 23:58:15 -0600

    Use more pattern matching
    Also make the semantics of reshape more similar to those of APL with
    respect to more/less data expected than given.

--- interpreter.hs
+++ interpreter.hs
@@ -3,12 +3,13 @@
 
 -- Define binary math operators on lists
 opI :: (a -> a -> a) -> [a] -> [a] -> [a]
-opI f (x:[]) (y:[]) = [f x y]
-opI f (x:xs) (y:[]) = (f x y : opI f xs [y])
-opI f (x:[]) (y:ys) = (f x y : opI f [x] ys)
-opI f (x:xs) (y:ys)
-    | length xs == length ys = (f x y : opI f xs ys)
-    | otherwise = error "Mismatched lengths"
+opI f xs (y:[]) = map (\x -> f x y) xs
+opI f (x:[]) ys = map (\y -> f x y) ys
+opI f x y = opI' f x y
+opI' f [] [] = []
+opI' f [] _ = error "Mismatched list lengths"
+opI' f _ [] = error "Mismatched list lengths"
+opI' f (x:xs) (y:ys) = (f x y : opI' f xs ys)
 
 addI = opI (+)
 subI = opI (-)
@@ -18,10 +19,13 @@ divI = opI div
 -- reshape: turn a 1-dimensional list into an x by y table
 -- This is not general enough -- consider rank 3 arrays
 reshape :: Int -> Int -> [a] -> [[a]]
-reshape x y t
-    | x == 1 && length t == y = [take y t]
-    | x > 1 && length t > y = [take y t] ++ reshape (x-1) y (drop y t)
-    | otherwise = error "Invalid dimensions"
+reshape 0 _ _ = []
+reshape x y t =
+    let iter 0 _ l = []
+        iter x y l = [take y l] ++ reshape (x-1) y (drop y l)
+    in  case (signum(x), signum(y)) of
+             (1, 1) -> iter x y (cycle t)
+             (_, _) -> error "Invalid dimensions"
 
 -- dimT: Get the dimensions of a table
 -- This assumes that the table is well-formed...
@@ -31,6 +35,5 @@ dimT (row:rows) = (length rows + 1, length row)
 -- join: Combine two tables with the same number of rows
 join :: [[a]] -> [[a]] -> [[a]]
 join (x:[]) (y:[]) = [x ++ y]
-join (x:xs) (y:ys)
-    | length xs == length ys = [x ++ y] ++ join xs ys
-    | otherwise = error "Mismatched number of rows"
+join (x:xs) (y:ys) = [x ++ y] ++ join xs ys
+join _ _ = error "Mismatched number of rows"
